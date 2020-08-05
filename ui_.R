e502b83987022685cc0b0b4dec73733a66581868
Slava the Great <slava.tsarev@gmail.com>
2020-08-05 14:09:52 +0100

    default values corrected a bit

--- ui_.R
+++ ui_.R
@@ -11,11 +11,11 @@ ui <- dashboardPage(
     title = "Drunks"
   ),
   dashboardSidebar(
-     numericInput(inputId = "nDrunks", label = "Drunks", value = 1, min = 0, step = 1),
-     numericInput(inputId = "nSteps", label = "Steps", value = 10000, min = 1, step = 1),
+     numericInput(inputId = "nDrunks", label = "Drunks", value = 10, min = 0, step = 1),
+     numericInput(inputId = "nSteps", label = "Steps", value = 5000, min = 1, step = 1),
      numericInput(inputId = "seed", label = "Seed", value = 19, min = 1, step = 1),
      numericInput(inputId = "policeMin", label = "Police Min", value = 1, min = 0, step = 1),
-     numericInput(inputId = "policeConcentration", label = "Police Concentration", value = 0.001, min = 0, max = 1),
+     numericInput(inputId = "policeConcentration", label = "Police Concentration", value = 0.0005, min = 0, max = 1),
      numericInput(inputId = "chartHeight", label = "Chart Height, px", value = 600, min = 200, max = 5000, step = 50),
      materialSwitch(inputId = "finalPositionOnly", label = "Show Final Position Only", right = TRUE, value = FALSE, inline = FALSE),
      materialSwitch(inputId = "policeAreBlind", label = "Police Are Blind", right = TRUE, value = FALSE, inline = FALSE)
